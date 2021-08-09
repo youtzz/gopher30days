723b48aa709bde678f1495c65fa34792181a3cb9
lilongjie <longjie.li@ppiaas.cn>
2021-08-09 16:21:59 +0800

    feat: day4
    day4

---
+++ algorithm/binarysearch/61_Search_Range/solution.go
@@ -0,0 +1,50 @@
+package binarysearch
+
+// first solution
+func searchRange(nums []int, target int) []int {
+	res := make([]int, 2)
+	res[0], res[1] = -1, -1
+	if len(nums) == 0 {
+		return res
+	}
+
+	// 第一次二分搜索，查找第一次出现target的位置
+	left, right := 0, len(nums)-1
+	for left+1 < right {
+		mid := (right-left)>>1 + left
+		if nums[mid] < target {
+			left = mid
+		} else if nums[mid] > target {
+			right = mid
+		} else {
+			right = mid
+		}
+	}
+	if nums[left] == target {
+		res[0] = left
+	} else if nums[right] == target {
+		res[0] = right
+	} else {
+		return res
+	}
+
+	// 第二次二分搜索，查找第二次出现target的位置
+	left, right = 0, len(nums)-1
+	for left+1 < right {
+		mid := (right-left)>>1 + left
+		if nums[mid] < target {
+			left = mid
+		} else if nums[mid] > target {
+			right = mid
+		} else {
+			left = mid
+		}
+	}
+	if nums[right] == target {
+		res[1] = right
+	} else if nums[left] == target {
+		res[1] = left
+	}
+
+	return res
+}
---
+++ algorithm/binarysearch/61_Search_Range/solution_test.go
@@ -0,0 +1,49 @@
+package binarysearch
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_searchRange(t *testing.T) {
+	type args struct {
+		nums   []int
+		target int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{
+			name: "1",
+			args: args{
+				nums:   []int{5, 7, 7, 8, 8, 10},
+				target: 8,
+			},
+			want: []int{3, 4},
+		},
+		{
+			name: "2",
+			args: args{
+				nums:   []int{},
+				target: 9,
+			},
+			want: []int{-1, -1},
+		},
+		{name: "3",
+			args: args{
+				nums:   []int{5, 5, 5, 5, 5, 5, 5, 5, 5, 5},
+				target: 5,
+			},
+			want: []int{0, 9},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := searchRange(tt.args.nums, tt.args.target); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("searchRange() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
